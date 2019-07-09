64ef05e2ab18d495b4cf10f97a8107577b342d7d
Luciano Sacramento <lucianorodrigons@gmail.com>
2019-07-09 14:03:11 -0700

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Binarysharp.MemoryManagement.MemorySharp;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+      
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
